5f2130d05aa1a18da21a4dbbc8f0de67af5a34f4
Maniteja Nandana <manitejanmt@gmail.com>
2016-08-11 08:53:17 +0530

    Add main class

--- Main.java
+++ Main.java
@@ -2,40 +2,47 @@ package card;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class Main {
 
 	private static int MIN_LIMIT = 1;
 	private static int MAX_LIMIT = 13;
-	private static int NO_OF_PLAYERS = 4;
+	private static int NO_OF_SUITS = 4;
 	
 	public static void main(String[] args) {
+		ArrayList<Card> cards = new ArrayList<>();
 		int noOfCards = MAX_LIMIT - MIN_LIMIT + 1;
-		ArrayList<Hand> hands = new ArrayList<>();
-		for(int player=1; player<=NO_OF_PLAYERS; player++) {
-			Hand hand = new Hand(noOfCards);
-	 		for(int value=MIN_LIMIT; value<MAX_LIMIT; value++) {
-				for(Suit suit : Suit.values()) {
-					Card card = new Card(value, suit);
-					hand.addCard(card);
-				}
+ 		for(int value=MIN_LIMIT; value<MAX_LIMIT; value++) {
+			for(Suit suit : Suit.values()) {
+				Card card = new Card(value, suit);
+				cards.add(card);
 			}
-	 		hands.add(hand);
 		}
-		for(Hand hand: hands) {
-//			System.out.println(hand.getListOfCards().size());
-			for(Card card : hand.getListOfCards()) {
-				System.out.print(card.getValue() +  " ");
-			}
-			System.out.println();
+		Collections.shuffle(cards);
+		Random rnd = new Random();
+		ArrayList<Card> rummyCards = new ArrayList<>(); 
+		for(int value=MIN_LIMIT; value<MAX_LIMIT; value++) {
+			rummyCards.add(cards.remove(rnd.nextInt(cards.size())));
+		}
+		
+		for(Card card: cards) {
+			System.out.print(card.getValue() + " " + card.getSuit()+"|");
 		}
 		System.out.println();
-		for(Hand hand: hands) {
-			for(Card card : hand.getSortedCards()) {
-				System.out.print(card.getValue() +  " ");
-			}
-			System.out.println();
+		ArrayList<Card> newCards = new ArrayList<>();
+		newCards = Card.getSortedCardsByValue(rummyCards);
+		for(Card card: newCards) {
+			System.out.print(card.getValue() + " " + card.getSuit()+"|");
 		}
 		System.out.println();
+		cards = Card.getSortedCardsBySuit(rummyCards);
+		for(Card card: cards) {
+			System.out.print(card.getValue() + " " + card.getSuit()+"|");
+		}
+		System.out.println();
+//		Rummy rummy = new Rummy(1, 1, rummyCards);
+//		rummy.calculate();
+			
 	}
 }
